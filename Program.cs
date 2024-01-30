841cefee4d522a5d42c357bd33a40e0a173f7634
Milosz Mirek <miloszmirekfilm@gmail.com>
2024-01-30 17:34:18 +0100

    open in window

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Zadam ci 5 prostych pytań");
+Console.WriteLine("Zadam ci 5 prostych lub trudnych pytań");
+Console.WriteLine("Jeśli jesteś gotowy to kliknij enter");
+Console.ReadLine();
