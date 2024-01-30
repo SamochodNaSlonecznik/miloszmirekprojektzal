86006c68d073fa5e2aa0ba13c3996eea2dccf7b4
Milosz Mirek <miloszmirekfilm@gmail.com>
2024-01-30 20:45:20 +0100

    clean up1

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
+﻿
         Console.WriteLine("Ill ask you 5 simple questions, in order to be a citizen of Chrzanów you must answer at least 3 correctly");
+        Console.WriteLine("------------------------------------");
 
         Console.WriteLine("Who's the current major of Chrzanów?");
-        string firstPlayerAnswer = Console.ReadLine();
+        string? firstPlayerAnswer = Console.ReadLine();
         if (firstPlayerAnswer == "Robert Maciaszek")
         {
             Console.WriteLine("Good, that was an easy question, next one will be harder");
+            Console.WriteLine("------------------------------------");
         }
         else
         {
             Console.WriteLine("What? Of course not. You're trying to a citizen of Chrzanów but you don't even know who's the mayor.");
             Console.WriteLine("I doubt you will be able to answer the next question. Well here goes nothing");
+            Console.WriteLine("------------------------------------");
         }
 
         // 4. count the correct answers
@@ -21,5 +21,3 @@
 
         Console.WriteLine("Dziękuje za test, jesteś teraz obywatelem miasta Chrzanowa");
         Console.ReadLine();
-    }
-}
